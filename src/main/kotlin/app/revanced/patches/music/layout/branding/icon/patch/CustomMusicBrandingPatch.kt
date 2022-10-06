143d26d2fd666908163b80cdd96f9b7cafb30239
LeddaZ <leonardoledda@gmail.com>
2022-10-06 22:16:11 +0200

    fix: adapt CustomMusicBrandingPatch to new patcher

--- src/main/kotlin/app/revanced/patches/music/layout/branding/icon/patch/CustomMusicBrandingPatch.kt
+++ src/main/kotlin/app/revanced/patches/music/layout/branding/icon/patch/CustomMusicBrandingPatch.kt
@@ -3,26 +3,29 @@ package app.revanced.patches.music.layout.branding.icon.patch
 import app.revanced.patcher.annotation.Description
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
-import app.revanced.patcher.data.impl.ResourceData
-import app.revanced.patcher.patch.PatchResult
-import app.revanced.patcher.patch.PatchResultError
-import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.data.ResourceContext
+import app.revanced.patcher.patch.*
 import app.revanced.patcher.patch.annotations.DependsOn
 import app.revanced.patcher.patch.annotations.Patch
-import app.revanced.patcher.patch.impl.ResourcePatch
 import app.revanced.patches.music.layout.branding.icon.annotations.CustomMusicBrandingCompatibility
+import app.revanced.patches.youtube.misc.manifest.patch.FixLocaleConfigErrorPatch
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
 import java.nio.file.Files
 
 @Patch
+@DependsOn([FixLocaleConfigErrorPatch::class])
 @Name("custom-music-branding")
-@Description("Changes the YouTube Music launcher icon to be ReVanced's.")
+@Description("Changes the YouTube Music launcher icon and name to your choice (defaults to ReVanced Music).")
 @CustomMusicBrandingCompatibility
 @Version("0.0.1")
-class CustomMusicBrandingPatch : ResourcePatch() {
-    override fun execute(data: ResourceData): PatchResult {
-        val resDirectory = data["res"]
+class CustomMusicBrandingPatch : ResourcePatch {
+    override fun execute(context: ResourceContext): PatchResult {
+        val resDirectory = context["res"]
         if (!resDirectory.isDirectory) return PatchResultError("The res folder can not be found.")
 
+        // Icon branding
         val iconNames = arrayOf(
             "adaptiveproduct_youtube_music_background_color_108",
             "adaptiveproduct_youtube_music_foreground_color_108",
@@ -37,17 +40,57 @@ class CustomMusicBrandingPatch : ResourcePatch() {
             "hdpi" to 72,
             "mdpi" to 48
         ).forEach { (iconDirectory, size) ->
-            iconNames.forEach iconLoop@{ iconName ->
-                val iconFile = this.javaClass.classLoader.getResourceAsStream("branding/music/$size/$iconName.png")
+            iconNames.forEach { iconName ->
+                val iconFile = getIconStream("branding/music/$size/$iconName.png")
                     ?: return PatchResultError("The icon $iconName can not be found.")
 
                 Files.write(
                     resDirectory.resolve("mipmap-$iconDirectory").resolve("$iconName.png").toPath(),
-                    iconFile.readAllBytes()
+                    iconFile.readBytes()
                 )
             }
         }
 
+        // Name branding
+        val manifest = context["AndroidManifest.xml"]
+        manifest.writeText(
+            manifest.readText()
+                .replace(
+                    "android:label=\"@string/application_name",
+                    "android:label=\"$appName"
+                )
+        )
+
         return PatchResultSuccess()
+    }
+
+    private fun getIconStream(iconPath: String): InputStream? {
+        if (appIconPath == null) {
+            return this.javaClass.classLoader.getResourceAsStream(iconPath)
+        }
+        val file = File(appIconPath!!).resolve(iconPath)
+        if (!file.exists()) return null
+        return FileInputStream(file)
+    }
+
+    companion object : OptionsContainer() {
+        private var appName: String? by option(
+            PatchOption.StringOption(
+                key = "appName",
+                default = "ReVanced Music",
+                title = "Application Name",
+                description = "The name of the application it will show on your home screen.",
+                required = true
+            )
+        )
+
+        private var appIconPath: String? by option(
+            PatchOption.StringOption(
+                key = "appIconPath",
+                default = null,
+                title = "Application Icon Path",
+                description = "A path to the icon of the application."
+            )
+        )
     }
 }
